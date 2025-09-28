96bad37b8c3cb88eb7f510e1796e82e09dd0d311
FMartineezz <facmartinez@frba.utn.edu.ar>
2025-09-27 21:02:48 -0300

    Metrics datadog Percentil

--- src/main/java/ar/edu/utn/dds/k3003/controller/SolicitudController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/SolicitudController.java
@@ -54,7 +54,7 @@ public class SolicitudController {
                 .register(this.registry);
         this.responseAplicationTimer = Timer.builder("solicitudes_response_application_timer")
                 .description("Tiempo que tarda en responder a la app que nos consume")
-                .publishPercentiles(95)
+                .publishPercentiles(0.95)
                 .register(this.registry);
     }
 
